c72912c5cad0ede6cd9b7a8ac3b6de4ca6adc021
myroot <myroot@nb.at.home>
2025-05-13 20:28:43 +0800

    add mdns local domain

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
   <UButtonGroup orientation='vertical'>
-    <UButton to='http://orangepizero2w:3000' target='_blank' variant='outline' size='xl' color='neutral'>
+    <UButton to='http://orangepizero2w.local:3000' target='_blank' variant='outline' size='xl' color='neutral'>
       signalk
     </UButton>
     <UButton to='/winch' target='_blank' variant='outline' size='xl' color='neutral'>
       winch
     </UButton>
-    <UButton to='http://ydnr' target='_blank' variant='outline' size='xl' color='neutral'>
+    <UButton to='http://ydnr.local' target='_blank' variant='outline' size='xl' color='neutral'>
       ydnr
     </UButton>
-    <UButton to='http://autopilot' target='_blank' variant='outline' size='xl' color='neutral'>
+    <UButton to='http://autopilot.local' target='_blank' variant='outline' size='xl' color='neutral'>
       autopilot
     </UButton>
   </UButtonGroup>
